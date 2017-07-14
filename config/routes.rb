fbe44b965c341c3891aca5c80c9668864bd14092
affiliatedashboardatl <kbumpus@mac-kbumpus.cb.careerbuilder.com>
2017-07-14 18:08:59 -0400

    so much stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'cj/performance'
-  get 'dashboard/home'
+	devise_for :users, :controllers => { registrations: 'registrations' }
+	get 'cj/performance'
+	get 'dashboard/home'
+	get 'welcome', to: 'landing#show'
 
-  root to: 'dashboard#home'
+	root to: 'dashboard#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
